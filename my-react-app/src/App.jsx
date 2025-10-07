bbd7f57a0708f123a280779c4fab295af5f9b6bb
krnlshh <Krunal.RajeshbhaiShah@thomsonreuters.com>
2025-10-07 20:56:39 +0530

    main(react project)

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import MyButton from './MyButton'
+import './style/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <MyButton />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
---
+++ my-react-app/src/MyApp.jsx
@@ -0,0 +1,11 @@
+// MyApp.jsx - Main app component that nests MyButton
+import MyButton from './MyButton'
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
